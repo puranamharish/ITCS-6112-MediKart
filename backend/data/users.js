a3a0295236d49ecc17648b8f38616d8dd8a5e179
puranamharish <94148401+puranamharish@users.noreply.github.com>
2021-12-06 23:38:27 -0500

    Update users.js

--- backend/data/users.js
+++ backend/data/users.js
@@ -5,30 +5,14 @@ const users = [
     name: 'Mahender',
     email: 'mahi@gmail.com',
     password: bcrypt.hashSync('test123', 10),
-    isVendor: false
+    isVendor: true
   },
   {
     name: 'Harish',
     email: 'hari@gmail.com',
     password: bcrypt.hashSync('test123', 10),
-    isVendor: true
-  },
-  // {
-  //   name: 'Admin User',
-  //   email: 'admin@example.com',
-  //   password: bcrypt.hashSync('123456', 10),
-  //   isAdmin: true,
-  // },
-  // {
-  //   name: 'John Doe',
-  //   email: 'john@example.com',
-  //   password: bcrypt.hashSync('123456', 10),
-  // },
-  // {
-  //   name: 'Jane Doe',
-  //   email: 'jane@example.com',
-  //   password: bcrypt.hashSync('123456', 10),
-  // },
+    isVendor: false
+  }
 ]
 
 export default users
